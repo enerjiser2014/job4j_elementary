10fed6635e423389e1e9900f7a21a1d594771934
Jagupov Jurii <project@idefclub.ru>
2020-10-13 17:32:31 +0300

    6. Идеальный вес [#383963]

---
+++ src/ru/job4j/calculator/Fit.java
@@ -0,0 +1,22 @@
+package ru.job4j.calculator;
+
+public class Fit {
+
+    public static double manWeight(short height) {
+        return (height - 100) * 1.15;
+    }
+
+    public static double womanWeight(short height) {
+        return (height - 110) * 0.85;
+    }
+
+    public static void main(String[] args) {
+        short height = 187;
+        double man = Fit.manWeight(height);
+        System.out.println("Man " + height + " is " + man);
+
+        double woman = Fit.womanWeight(height);
+        System.out.println("woman " +height + " is " + woman);
+    }
+
+}

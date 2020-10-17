870d99b18790c402b97c7dd227eee0a23e1ba7f5
Jagupov Jurii <project@idefclub.ru>
2020-10-17 13:35:36 +0300

    4. Тесты для стороны прямоугольника. [#383999]
    fix sqArea

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -3,7 +3,7 @@ package ru.job4j.condition;
 public class SqArea {
     public static double square(int p, int k) {
         // double s = k * p / (2*k + 2);
-        double h = p / (2 * (k+1));
+        double h = p / (double) (2 * (k+1));
         double l = h * k;
         return l * h; // s
     }
@@ -11,6 +11,7 @@ public class SqArea {
     public static void main(String[] args) {
         double result1 = SqArea.square(6,2);
         System.out.println("p = 4, k = 1; s = " + result1);
-        System.out.println("p = 6, k = 2; s = " + result1);
+        double result2 = SqArea.square(6,3);
+        System.out.println("p = 6, k = 2; s = " + result2);
     }
 }

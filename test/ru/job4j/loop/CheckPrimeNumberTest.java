5d4d293294d77cf13a4b48ca0885195eb79c806c
Jagupov Jurii <project@idefclub.ru>
2020-10-21 21:57:21 +0300

    5.5. Простое число [#384023]

---
+++ test/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class CheckPrimeNumberTest {
+    @Test
+    public void when5() {
+        boolean rsl = CheckPrimeNumber.check(5);
+        assertThat(rsl, is(true));
+    }
+
+    @Test
+    public void when4() {
+        boolean rsl = CheckPrimeNumber.check(4);
+        assertThat(rsl, is(false));
+    }
+
+    @Test
+    public void when1() {
+        boolean rsl = CheckPrimeNumber.check(1);
+        assertThat(rsl, is(true));
+    }
+
+    @Test
+    public void when13() {
+        boolean rsl = CheckPrimeNumber.check(13);
+        assertThat(rsl, is(true));
+    }
+
+    @Test
+    public void when3571() {
+        boolean rsl = CheckPrimeNumber.check(3571);
+        assertThat(rsl, is(true));
+    }
+
+    @Test
+    public void when65535() {
+        boolean rsl = CheckPrimeNumber.check(65535);
+        assertThat(rsl, is(false));
+    }
+}

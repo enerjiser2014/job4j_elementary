f81072bd816f46440b1279aa9bdc994be0987d5e
Jagupov Jurii <project@idefclub.ru>
2020-10-22 21:08:37 +0300

    6.0.2. Заполнение массива. [#383944]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -4,9 +4,15 @@ public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
+        String[] names = new String[4];
         float[] prices = new float[40];
         System.out.println("Размер массива ages равен: " + ages.length);
         System.out.println("Размер массива surnames равен: " + surnames.length);
         System.out.println("Размер массива prices равен: " + prices.length);
+
+        names[0] = "Petr Arsentev";
+        names[1] = "Jagupov Jurii";
+        names[2] = "Vladimir Lenin";
+        names[3] = "Sergey Krolev";
     }
 }

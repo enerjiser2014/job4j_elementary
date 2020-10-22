b0396c5ddcdeefe92d336c04c5f48b7fb7e42a80
Jagupov Jurii <project@idefclub.ru>
2020-10-22 20:34:29 +0300

    fix prime numbers

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,14 +2,13 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = true;
+        boolean prime = number == 1 ? false : true;
         for (int i=2; i < number; i++){
             if (number % i == 0) {
                 prime = false;
                 break;
             }
         }
-        if (number == 1) prime = false;
         return prime;
     }
 

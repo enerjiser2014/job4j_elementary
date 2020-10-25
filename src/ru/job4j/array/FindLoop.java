9837440a7a85345bb8b61542c8c506deba76645a
Jagupov Jurii <project@idefclub.ru>
2020-10-25 23:12:28 +0300

    6.5. Поиск индекса в диапазоне. [#383937]

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -1,9 +1,10 @@
 package ru.job4j.array;
 
 public class FindLoop {
-    public static int indexOf(int[] data, int el) {
+    public static int indexOf(int[] data, int el, int start, int finish) {
         int rst = -1;
-        for (int index = 0; index < data.length; index++) {
+        int endloop = finish <= data.length-1 ? finish : data.length-1;
+        for (int index = start; index <= endloop; index++) {
             if (data[index] == el) {
                 rst = index;
                 break;
--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -3,16 +3,18 @@ package ru.job4j.array;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-import  static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class FindLoopTest {
 
     @Test
-    public void whenArrayHas5Then0() {
+    public void whenArrayHas5Then0Finish8() {
         int[] input = {5, 4, 3, 2};
+        int start = 0;
+        int finish = 8;
         int value = 5;
-        int result = FindLoop.indexOf(input, value);
+        int result = FindLoop.indexOf(input, value, start, finish);
         int expect = 0;
         assertThat(result, is(expect));
     }
@@ -20,8 +22,10 @@ public class FindLoopTest {
     @Test
     public void whenArrayHas3Then2() {
         int[] input = {5, 4, 3, 2};
+        int start = 1;
+        int finish = 2;
         int value = 3;
-        int result = FindLoop.indexOf(input, value);
+        int result = FindLoop.indexOf(input, value, start, finish);
         int expect = 2;
         assertThat(result, is(expect));
     }
@@ -29,17 +33,32 @@ public class FindLoopTest {
     @Test
     public void whenArrayHas2Then3() {
         int[] input = {5, 4, 3, 2};
+        int start = 2;
+        int finish = 3;
         int value = 2;
-        int result = FindLoop.indexOf(input, value);
+        int result = FindLoop.indexOf(input, value, start, finish);
         int expect = 3;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenArrayHas2ThenMinusOne() {
+        int[] input = {5, 4, 3, 2};
+        int value = 2;
+        int start = 1;
+        int finish = 2;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = -1;
         assertThat(result, is(expect));
     }
 
     @Test
     public void whenArrayHas8ThenMinusOne() {
         int[] input = {5, 4, 3, 2};
+        int start = 0;
+        int finish = 3;
         int value = 8;
-        int result = FindLoop.indexOf(input, value);
+        int result = FindLoop.indexOf(input, value, start, finish);
         int expect = -1;
         assertThat(result, is(expect));
     }

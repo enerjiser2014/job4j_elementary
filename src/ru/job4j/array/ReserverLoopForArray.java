665b18b149fc71267b6a34542b3a41ff94b10e70
Jagupov Jurii <project@idefclub.ru>
2020-10-26 09:43:47 +0300

    fix reserve loop and even check in generated index

--- src/ru/job4j/array/ReserverLoopForArray.java
+++ src/ru/job4j/array/ReserverLoopForArray.java
@@ -3,10 +3,11 @@ package ru.job4j.array;
 public class ReserverLoopForArray {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5, 6, 7};
-
+        int index;
         for (int i = 0; i < numbers.length; i++) {
-            if (i % 2 == 0 && i != 0) {
-                System.out.println(numbers[i]);
+            index = numbers.length-i-1;
+            if ( index % 2 == 0 && index != 0) {
+                System.out.println(numbers[index]);
             }
 
         }

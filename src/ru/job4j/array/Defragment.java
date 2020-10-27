a461d5fcc245d8d204e5c36f9e637a65d7544b70
Jagupov Jurii <project@idefclub.ru>
2020-10-27 20:21:07 +0300

    6.8. Дефрагментация массива [#383958]

---
+++ src/ru/job4j/array/Defragment.java
@@ -0,0 +1,29 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length ; index++) {
+            if (array[index] == null) {
+                    for (int j = index; j < array.length; j++) {
+                        if (array[j] != null) {
+                            array[index] = array[j];
+                            array[j] = null;
+                            break;
+                        }
+                    }
+                }
+
+        }
+
+        return array;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        System.out.println();
+        for (int index = 0; index < compressed.length; index++) {
+            System.out.print(compressed[index] + " ");
+        }
+    }
+}

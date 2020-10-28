a684dfa6b85e6aae46c6b9c5c3c2525664f6ce68
Jagupov Jurii <project@idefclub.ru>
2020-10-28 15:01:40 +0300

    matrixarrays fix add break in isWin method and test

---
+++ src/ru/job4j/sort/Machine.java
@@ -0,0 +1,21 @@
+package ru.job4j.sort;
+
+import java.util.Arrays;
+
+public class Machine {
+    private final int[] COINS = {10, 5, 2, 1};
+
+    public int[] change(int money, int price) {
+        int[] rsl = new int[100];
+        int coinsSum = 0;
+        int size = 0;
+        for (int i = 0; i < COINS.length; i++) {
+            while (coinsSum + price + COINS[i] <= money) {
+                rsl[size] = COINS[i];
+                coinsSum += COINS[i];
+                size++;
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+}

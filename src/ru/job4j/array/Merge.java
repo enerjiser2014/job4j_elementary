8d6e9806be77f35547122242546a410575b70002
Jagupov Jurii <project@idefclub.ru>
2020-12-01 12:57:05 +0300

    arrayMerge

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -3,6 +3,25 @@ package ru.job4j.array;
 public class Merge {
     public static int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
+        boolean arrNotBreak;
+        for (int z = 0, i = 0, j = 0; z < rsl.length; z++) {
+            arrNotBreak = i < left.length && j < right.length;
+            if (arrNotBreak) {
+                rsl[z] = left[i] < right[j] ? left[i++] : right[j++];
+            }
+            if (!arrNotBreak && i < left.length) {
+                rsl[z] = left[i++];
+            }
+            if (!arrNotBreak && j < right.length) {
+                rsl[z] = right[j++];
+            }
+        }
+        return rsl;
+    }
+
+
+    public static int[] merge2(int[] left, int[] right) {
+        int[] rsl = new int[left.length + right.length];
         int i = 0, j = 0, z = 0;
         while (i < left.length && j < right.length) {
             rsl[z++] = left[i] < right[j] ? left[i++] : right[j++];

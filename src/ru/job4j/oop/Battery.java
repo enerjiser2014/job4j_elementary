2536d9f3581dcafbdaa61d3f9b265c3273479490
Jagupov Jurii <project@idefclub.ru>
2020-12-12 17:13:39 +0300

    1.7. Состояние объекта [#384129]

---
+++ src/ru/job4j/oop/Battery.java
@@ -0,0 +1,26 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int power;
+
+    public Battery(int pw) {
+        this.power = pw;
+    }
+    public void exchange(Battery another){
+        this.power += another.power;
+        another.power = 0;
+    }
+    public void show(){
+        System.out.println("power is: " + this.power);
+    }
+
+    public static void main(String[] args) {
+        Battery bat1 = new Battery(50);
+        Battery bat2 = new Battery(50);
+        bat1.show();
+        bat2.show();
+        bat1.exchange(bat2);
+        bat1.show();
+        bat2.show();
+    }
+}

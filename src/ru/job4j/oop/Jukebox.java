f7533c0298a4abbc1d8a912a9b5d084141079059
Jagupov Jurii <project@idefclub.ru>
2021-01-02 10:21:40 +0300

    Вызов метода с аргументами. [#384126] fix style

--- src/ru/job4j/oop/Jukebox.java
+++ src/ru/job4j/oop/Jukebox.java
@@ -3,9 +3,9 @@ package ru.job4j.oop;
 public class Jukebox {
     private String song1 = "Пусть бегут неуклюже...";
     private String song2 = "Спокойной ночи...";
-    private String NotFound = "Песня не найдена";
+    private String notFound = "Песня не найдена";
     public void music(int position){
-        String rsl = NotFound;
+        String rsl = notFound;
         if (position == 1) {
             rsl = song1;
         }
